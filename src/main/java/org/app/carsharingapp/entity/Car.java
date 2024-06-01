2fde1ce2dddb5c23e9e2def40843ae9a071687fc
Stepan <petrivs572@gmail.com>
2024-06-01 19:54:00 +0300

    Added car entity

---
+++ src/main/java/org/app/carsharingapp/entity/Car.java
@@ -0,0 +1,43 @@
+package org.app.carsharingapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity(name = "cars")
+@Data
+@SQLDelete(sql = "UPDATE addresses SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String model;
+    @Column(nullable = false)
+    private String brand;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+    @Column(nullable = false)
+    private Integer inventory;
+    @Column(nullable = false)
+    private BigDecimal dailyFee;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    public enum Type {
+        SEDAN,
+        SUV,
+        HATCHBACK,
+        UNIVERSAL
+    }
+}
