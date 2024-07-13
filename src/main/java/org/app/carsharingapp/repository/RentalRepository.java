18d63e0c044932696c50cd18ff0f51a538fd7494
Stepan <petrivs572@gmail.com>
2024-07-13 10:27:44 +0300

    Fixed rental repository

--- src/main/java/org/app/carsharingapp/repository/RentalRepository.java
+++ src/main/java/org/app/carsharingapp/repository/RentalRepository.java
@@ -3,8 +3,9 @@ package org.app.carsharingapp.repository;
 import java.util.List;
 import org.app.carsharingapp.entity.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RentalRepository extends JpaRepository<Rental, Long> {
+public interface RentalRepository extends JpaRepository<Rental, Long>, JpaSpecificationExecutor<Rental> {
     List<Rental> findAllByUserId(Long userId);
 
     List<Rental> findAllByUserIdAndCarId(Long userId, Long carId);
