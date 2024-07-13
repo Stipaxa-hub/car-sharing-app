d30ceba3428fa45c4bdac3fb58dbb198534ad3eb
Stepan <petrivs572@gmail.com>
2024-07-13 10:31:19 +0300

    Fixed checkstyle

--- src/main/java/org/app/carsharingapp/repository/RentalRepository.java
+++ src/main/java/org/app/carsharingapp/repository/RentalRepository.java
@@ -5,7 +5,8 @@ import org.app.carsharingapp.entity.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RentalRepository extends JpaRepository<Rental, Long>, JpaSpecificationExecutor<Rental> {
+public interface RentalRepository extends JpaRepository<Rental, Long>,
+        JpaSpecificationExecutor<Rental> {
     List<Rental> findAllByUserId(Long userId);
 
     List<Rental> findAllByUserIdAndCarId(Long userId, Long carId);
